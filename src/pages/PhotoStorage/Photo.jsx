11cc50e0939e76730cca3b79032394e05103bf4c
woojenoh <woojenoh@gmail.com>
2021-05-11 22:37:03 +0900

    PhotoStorage, Photo 마크업 추가
    - 임시로 추가

---
+++ src/pages/PhotoStorage/Photo.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const photoWrapperStyle = css({
+  '@media (min-width: 1200px)': {
+    width: '18%',
+    margin: '0 2.5% 2.5% 0',
+    '&:nth-of-type(5n)': {
+      marginRight: 0,
+    },
+  },
+  '@media (max-width: 1200px)': {
+    width: '22%',
+    margin: '0 4% 4% 0',
+    '&:nth-of-type(4n)': {
+      marginRight: 0,
+    },
+  },
+});
+
+const photoStyle = css({
+  position: 'relative',
+  paddingTop: '100%',
+  overflow: 'hidden',
+});
+
+const photoCenteredStyle = css({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  transform: 'translate(50%, 50%)',
+});
+
+const photoImgStyle = css({
+  transform: 'translate(-50%, -50%)',
+});
+
+const Photo = () => (
+  <div css={photoWrapperStyle}>
+    <div css={photoStyle}>
+      <div css={photoCenteredStyle}>
+        <img
+          css={photoImgStyle}
+          src="http://image.chosun.com/sitedata/image/201904/10/2019041001881_0.png"
+          alt="IU"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Photo;
--- src/pages/PhotoStorage/index.jsx
+++ src/pages/PhotoStorage/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import Photo from './Photo';
+
+const photoStorage = css({
+  display: 'flex',
+  padding: 50,
+  flexWrap: 'wrap',
+});
 
 const PhotoStorage = () => (
-  <div>PhotoStorage</div>
+  <div css={photoStorage}>
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+    <Photo />
+  </div>
 );
 
 export default PhotoStorage;
